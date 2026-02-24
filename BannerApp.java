67882b15c1f9b071f9aa50fe16990baa9b14dae0
gd1589-wq <gd1589@srmist.edu.in>
2026-02-24 18:37:26 +0530

    Create BannerApp.java

---
+++ BannerApp.java
@@ -0,0 +1,16 @@
+public class BannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("    ***       ***     ******       *****  ");
+        System.out.println("  **   **   **   **   **    **   **       ");
+        System.out.println(" **     ** **     **  **     ** **        ");
+        System.out.println(" **     ** **     **  **    **   **       ");
+        System.out.println(" **     ** **     **  ******      ***     ");
+        System.out.println(" **     ** **     **  **             **   ");
+        System.out.println(" **     ** **     **  **              **  ");
+        System.out.println("  **   **   **   **   **             **   ");
+        System.out.println("    ***       ***     **         *****    ");
+
+    }
+}
