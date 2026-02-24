5a2d34adfbbdd8bf97f907a46299469c318b5371
gd1589-wq <gd1589@srmist.edu.in>
2026-02-24 18:48:12 +0530

    Create OOPSBannerApp.java

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * @author YourName
+ * @version 1.0
+ * UC1 - Print OOPS to console
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
